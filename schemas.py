5a2971b77e9157c998a4a05b1fa73eb7c1e1734b
TalhaChougle <tchougle60@gmail.com>
2026-07-05 22:10:58 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -26,3 +26,23 @@ class CertificationUpdate(BaseModel):
     description: Optional[str] = None
     credential_url: Optional[str] = None
     modules: Optional[List[str]] = None
+
+
+class ResumeOut(BaseModel):
+    id: int
+    file_url: str
+    media_type: str
+
+    class Config:
+        from_attributes = True
+
+
+class DocOut(BaseModel):
+    id: int
+    title: str
+    description: str
+    file_url: str
+    media_type: str
+
+    class Config:
+        from_attributes = True
